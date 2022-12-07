e870e3c5b957b6ca56107662e236db75805aedb0
unknown <viktor.sterjovski@yahoo.com>
2022-12-07 18:54:47 +0100

    6esti vezbi

---
+++ Vezbi6/src/zadaca1/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        Gradovi gradovi = new Gradovi();
+
+        gradovi.DodadiGradovi();
+        gradovi.PecatiGradovi();
+
+    }
+}
+
---
+++ Vezbi6/src/zadaca2/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Drzavi drzavi = new Drzavi();
+
+        drzavi.DodadiDrzavi();
+        drzavi.PecatiDrzavi();
+
+    }
+}
